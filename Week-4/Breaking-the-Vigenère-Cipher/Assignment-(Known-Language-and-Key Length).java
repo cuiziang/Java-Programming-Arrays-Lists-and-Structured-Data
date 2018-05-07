12da1fc9cad6fc7fb86acb025e925b181eb68f1e
Ziang Cui <mrchoisss@gmail.com>
2018-05-06 23:06:00 -0400

    init w4c1

---
+++ Week-4/Breaking-the-Vigenère-Cipher/Assignment-(Known-Language-and-Key Length).java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+import edu.duke.*;
+
+public class VigenereBreaker {
+    public String sliceString(String message, int whichSlice, int totalSlices) {
+        String sliced = "";
+        for (int i = whichSlice; i < message.length(); i += totalSlices) {
+            sliced += message.charAt(i);
+        }
+        return sliced;
+    }
+
+    public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
+        int[] key = new int[klength];
+        CaesarCracker ccr = new CaesarCracker(mostCommon);
+        for (int i = 0; i < klength; i++) {
+            String m = sliceString(encrypted,i,klength);
+            int k = ccr.getKey(m);
+            key[i]=k;
+        }
+        return key;
+    }
+
+    public void breakVigenere() {
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+
+        VigenereBreaker vb = new VigenereBreaker();
+        int[] key = vb.tryKeyLength(message,4,'e');
+
+        VigenereCipher vc = new VigenereCipher(key);
+        for (int i : key) {
+            System.out.println(i);
+        }
+        System.out.println(vc.decrypt(message));
+    }
+}
